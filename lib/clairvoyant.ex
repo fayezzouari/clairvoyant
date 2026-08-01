fa444893ab08355355467d176c83ecbf6477299e
fayezzouari <fayez.zouari@insat.ucar.tn>
2026-08-01 23:37:16 +0200

    feat: add initial project files including formatter, gitignore, and basic module setup

---
+++ lib/clairvoyant.ex
@@ -0,0 +1,18 @@
+defmodule Clairvoyant do
+  @moduledoc """
+  Documentation for `Clairvoyant`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clairvoyant.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
