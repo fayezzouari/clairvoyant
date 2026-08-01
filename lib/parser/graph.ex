a36b2ba38830626428535e9709cd937b0e9f6486
fayezzouari <fayez.zouari@insat.ucar.tn>
2026-08-01 23:20:08 +0200

    feat: implement add_edge function to manage graph edges

--- lib/parser/graph.ex
+++ lib/parser/graph.ex
@@ -12,4 +12,13 @@ defmodule Clairvoyant.Graph do
   def add_node(%__MODULE__{nodes: nodes} = graph, name, version) do
     %{graph | nodes: Map.put(nodes, name, %{version: version})}
   end
+
+  @spec add_edge(t(), name(), name())::t()
+  def add_edge(%__MODULE__{} =graph, from, to) do
+    children = Map.get(graph.edges, from, [])
+    if to in children do
+      graph
+    else
+      %{graph | edges: Map.put(graph.edges, from, [to | children])}
+    end
 end
