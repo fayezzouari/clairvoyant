24f8b1cc14627a52b45c7ff688820f0e6796d1dd
fayezzouari <fayez.zouari@insat.ucar.tn>
2026-08-01 23:39:49 +0200

    refactor: update graph module to use maps for nodes and edges, improve type specifications

--- lib/parser/graph.ex
+++ lib/parser/graph.ex
@@ -1,20 +1,20 @@
 defmodule Clairvoyant.Graph do
-  defstruct nodes: [], edges: []
-  @type name: String.t()
+  defstruct nodes: %{}, edges: %{}
+  @type name :: String.t()
   @type t :: %__MODULE__{
-          nodes: [name()=>%{version: String.t()}],
-          edges: [{name()=>[name()]}]
+          nodes: %{optional(name()) => %{version: String.t()}},
+          edges: %{optional(name()) => [name()]}
         }
-  @spec new()::t()
-  def new() do:  %__MODULE__{}
+  @spec new() :: t()
+  def new(), do: %__MODULE__{}
 
-  @spec add_node(t(), name(), String.t())::t()
+  @spec add_node(t(), name(), String.t()) :: t()
   def add_node(%__MODULE__{nodes: nodes} = graph, name, version) do
     %{graph | nodes: Map.put(nodes, name, %{version: version})}
   end
 
-  @spec add_edge(t(), name(), name())::t()
-  def add_edge(%__MODULE__{} =graph, from, to) do
+  @spec add_edge(t(), name(), name()) :: t()
+  def add_edge(%__MODULE__{} = graph, from, to) do
     children = Map.get(graph.edges, from, [])
     if to in children do
       graph
@@ -23,17 +23,17 @@ defmodule Clairvoyant.Graph do
     end
   end
 
-  @spec children(t(), name())::[name()]
+  @spec children(t(), name()) :: [name()]
   def children(%__MODULE__{} = graph, name) do
     Map.get(graph.edges, name, [])
   end
 
-  @spec parents(t(), name())::[name()]
+  @spec parents(t(), name()) :: [name()]
   def parents(%__MODULE__{} = graph, name) do
     for {parent, children} <- graph.edges, name in children, do: parent
   end
 
-  @spec roots(t())::[name()]
+  @spec roots(t()) :: [name()]
   def roots(%__MODULE__{} = graph) do
     all_children = graph.edges |> Map.values() |> List.flatten() |> MapSet.new()
     graph.nodes
@@ -42,7 +42,7 @@ defmodule Clairvoyant.Graph do
     |> Enum.sort()
   end
 
-  @spec version(t(), name())::String.t() | nil
+  @spec version(t(), name()) :: String.t() | nil
   def version(%__MODULE__{} = graph, name) do
     case Map.get(graph.nodes, name) do
       %{version: version} -> version
